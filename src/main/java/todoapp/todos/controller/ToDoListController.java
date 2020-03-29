55423294f6a70492504da0f24869d9b8b2151173
Mox Muriz <mox.muriz@hoelderlin.com>
2020-03-29 11:04:58 +0200

    Rename todoService -> toDoListService

--- src/main/java/todoapp/todos/controller/ToDoListController.java
+++ src/main/java/todoapp/todos/controller/ToDoListController.java
@@ -18,47 +18,47 @@ import static todoapp.todos.controller.Constants.REST_API_BASE_URL;
 @RequestMapping(REST_API_BASE_URL + "/to-do-lists")
 public class ToDoListController {
 
-    private final ToDoListCrudService todoService;
+    private final ToDoListCrudService toDoListService;
 
     @Autowired
-    public ToDoListController(ToDoListCrudService todoService) {
-        this.todoService = todoService;
+    public ToDoListController(ToDoListCrudService toDoListService) {
+        this.toDoListService = toDoListService;
     }
 
     @GetMapping()
     public List<ToDoList> getTodoLists() {
-        return todoService.getTodoLists();
+        return toDoListService.getTodoLists();
     }
 
     @GetMapping(path = "/{id}")
     public ToDoList getTodoList(@PathVariable long id) throws ToDoListNotFoundException {
-        return todoService.getTodoList(id);
+        return toDoListService.getTodoList(id);
     }
 
     @GetMapping(path = "/{toDoListId}/to-dos")
     public List<ToDo> getTodos(@PathVariable long toDoListId) throws ToDoListNotFoundException {
-        return todoService.getToDos(toDoListId);
+        return toDoListService.getToDos(toDoListId);
     }
 
     @PostMapping()
     public ToDoList addToDoList(@RequestBody String listName) {
-        return todoService.createTodoList(listName);
+        return toDoListService.createTodoList(listName);
     }
 
     @PostMapping(path = "/{toDoListId}")
     public ToDo addTodo(@PathVariable long toDoListId, @RequestBody ToDo todo) throws ToDoListNotFoundException {
-        return todoService.addTodo(toDoListId, todo);
+        return toDoListService.addTodo(toDoListId, todo);
     }
 
     // TODO: Rename Todo -> ToDo
     @DeleteMapping(path = "/{toDoListId}")
     public ToDoList deleteToDoList(@PathVariable("toDoListId") long toDoListId) throws ToDoListNotFoundException {
-        return todoService.deleteToDoList(toDoListId);
+        return toDoListService.deleteToDoList(toDoListId);
     }
 
     @DeleteMapping(path = "/{toDoListId}/to-dos/{toDoId}")
     public ToDo deleteTodo(@PathVariable("toDoListId") long toDoListId, @PathVariable("toDoId") long toDoId) throws ToDoListNotFoundException, TodoNotFoundException {
-        return todoService.deleteToDo(toDoListId, toDoId);
+        return toDoListService.deleteToDo(toDoListId, toDoId);
     }
 
     @ExceptionHandler({ ToDoListNotFoundException.class, TodoNotFoundException.class })
